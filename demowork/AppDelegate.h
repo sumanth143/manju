bd59ef58781a908ae5d2653dd664b953126d45aa
student <student@achiever-6.local>
2017-09-01 17:13:03 +0530

    Initial Commit

---
+++ demowork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demowork
+//
+//  Created by student on 9/1/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
